c9164c52cd321dfd956456c16a3ba005faf15608
joker806 <joker806@gmail.com>
2012-04-08 21:18:06 +0200

    Working sending of template structure to the server, when TemplateEditor is saved

--- public/js/WebBuilder/lib/module/TemplatesManager/TemplateEditor.js
+++ public/js/WebBuilder/lib/module/TemplatesManager/TemplateEditor.js
@@ -1,66 +1,78 @@
 Ext.define( 'WebBuilder.module.TemplatesManager.TemplateEditor',
 {
 	extend : 'Ext.tab.Panel',
-	
+
 	mixins : {
 		editor : 'extAdmin.component.editor.DataEditorFeature'
 	},
-	
+
 	requires : [
 		'Ext.container.Container',
 		'WebBuilder.component.TemplateEditor'
 	],
-	
+
 	width  : 800,
 	height : 600,
-	
+
 	/**
 	 * Editor initialization
-	 * 
+	 *
 	 */
 	initComponent : function()
 	{
 		var me = this;
-		
+
+		me.nameField = Ext.create( 'Ext.form.field.Text', {
+			fieldLabel : 'Název',
+			name       : 'name'
+		});
+
+		me.templateEditor = Ext.create( 'WebBuilder.component.TemplateEditor', {
+			env : me.env
+		});
+
+
+
 		var generalTab = Ext.create( 'Ext.container.Container', {
 			title   : 'Obecné',
 			padding : 5,
-			
-			items : [{
-				xtype      : 'textfield',
-				fieldLabel : 'Název',
-				name       : 'name'
-			}]
+
+			items : [ me.nameField ]
 		});
-		
-		var canvasTab = Ext.create( 'WebBuilder.component.TemplateEditor', {
-			title  : 'Návrh šablony',
-			env    : me.env
+
+		var editorTab = Ext.apply( me.templateEditor, {
+			title  : 'Návrh šablony'
 		});
-		
-		
-		me.border = false;
-		me.items  = [ generalTab, canvasTab ];
-		
-		
-		me.title   = 'Úprava šablony';
-		me.iconCls = 'i-delete';
-		
+
+
+		Ext.apply( me, {
+			title   : 'Úprava šablony',
+			iconCls : 'i-delete',
+			border  : false,
+
+			items  : [ generalTab, editorTab ]
+		});
+
 		me.mixins.editor.constructor.call( me );
-		
+
 		me.callParent( arguments );
 	},
-	
+
 	setData : function( data )
 	{
 		console.log( 'set data ', data );
 	},
-	
+
 	getData : function()
 	{
-		return {};
+		var me = this;
+
+		return {
+			name     : me.nameField.getValue(),
+			template : me.templateEditor.getValue()
+		};
 	},
-	
+
 	isDirty : function()
 	{
 		return true;
