abe31f1bee7a4cd56c7e0464a711becb91d396d4
joker806 <joker806@gmail.com>
2012-04-16 15:45:33 +0200

    Close whole block & templates generation into one transaction

--- src/WebBuilder/BlocksGenerator.php
+++ src/WebBuilder/BlocksGenerator.php
@@ -45,63 +45,53 @@ class BlocksGenerator
 
 		echo "- registering block {$blockClassName}\n";
 
-		$this->database->beginTransaction();
+		// if block with given name exists, reuse ID
+		if( isset( $existingBlocks[ $blockClassName ] ) ) {
+			$blockID = $existingBlocks[ $blockClassName ];
 
-		try {
-			// if block with given name exists, reuse ID
-			if( isset( $existingBlocks[ $blockClassName ] ) ) {
-				$blockID = $existingBlocks[ $blockClassName ];
-
-			// otherwise insert new into DB
-			} else {
-				$this->database->exec( 'INSERT INTO '.self::TABLE_BLOCKS.' ( code_name ) VALUES ( '.$this->database->quote( $blockClassName ).' )' );
-				$blockID = $this->database->lastInsertId();
-			}
-
-			$validBlocks[] = $blockID;
-
-			// DATA DEPENDENCIES
-			$validDependencies = array();
-
-			$sql = 'SELECT ID, property FROM '.self::TABLE_REQUIREMENTS.' WHERE block_ID = '.$blockID;
-			foreach( $this->database->query( $sql ) as $r ) {
-				$existingDependencies[ $r['property'] ] = $r['ID'];
-			}
-
-			$dependencies = $blockClassName::requires();
-			if( $dependencies !== null ) {
-				foreach( $dependencies as $property => $dataType ) {
-
-					// if dependency with given name exists, reuse ID
-					if( isset( $existingDependencies[ $property ] ) ) {
-						$dependencyID = $existingDependencies[ $property ];
-						$this->database->exec( 'UPDATE '.self::TABLE_REQUIREMENTS.' SET data_type = '.$this->database->quote( $dataType )." WHERE ID = {$dependencyID}" );
-
-					// otherwise insert new into DB
-					} else {
-						$this->database->exec( 'INSERT INTO '.self::TABLE_REQUIREMENTS." ( block_ID, property, data_type ) VALUES ( {$blockID}, ".$this->database->quote( $property ).', '.$this->database->quote( $dataType ).' )' );
-						$dependencyID = $this->database->lastInsertId();
-					}
-
-					$validDependencies[] = $dependencyID;
-				}
-			}
-
-			// remove unknown dependencies within block
-			if( sizeof( $validDependencies ) == 0 ) {
-				$sql = 'DELETE FROM '.self::TABLE_REQUIREMENTS." WHERE block_ID = {$blockID}";
-			} else {
-				$sql = 'DELETE FROM '.self::TABLE_REQUIREMENTS." WHERE block_ID = {$blockID} AND ID NOT IN (".implode( ',', $validDependencies ).');';
-			}
-
-			$this->database->exec( $sql );
-
-		} catch( \DatabaseException $e ) {
-			$this->database->rollBack();
-			throw $e;
+		// otherwise insert new into DB
+		} else {
+			$this->database->exec( 'INSERT INTO '.self::TABLE_BLOCKS.' ( code_name ) VALUES ( '.$this->database->quote( $blockClassName ).' )' );
+			$blockID = $this->database->lastInsertId();
 		}
 
-		$this->database->commit();
+		$validBlocks[] = $blockID;
+
+		// DATA DEPENDENCIES
+		$validDependencies = array();
+
+		$sql = 'SELECT ID, property FROM '.self::TABLE_REQUIREMENTS.' WHERE block_ID = '.$blockID;
+		foreach( $this->database->query( $sql ) as $r ) {
+			$existingDependencies[ $r['property'] ] = $r['ID'];
+		}
+
+		$dependencies = $blockClassName::requires();
+		if( $dependencies !== null ) {
+			foreach( $dependencies as $property => $dataType ) {
+
+				// if dependency with given name exists, reuse ID
+				if( isset( $existingDependencies[ $property ] ) ) {
+					$dependencyID = $existingDependencies[ $property ];
+					$this->database->exec( 'UPDATE '.self::TABLE_REQUIREMENTS.' SET data_type = '.$this->database->quote( $dataType )." WHERE ID = {$dependencyID}" );
+
+				// otherwise insert new into DB
+				} else {
+					$this->database->exec( 'INSERT INTO '.self::TABLE_REQUIREMENTS." ( block_ID, property, data_type ) VALUES ( {$blockID}, ".$this->database->quote( $property ).', '.$this->database->quote( $dataType ).' )' );
+					$dependencyID = $this->database->lastInsertId();
+				}
+
+				$validDependencies[] = $dependencyID;
+			}
+		}
+
+		// remove unknown dependencies within block
+		if( sizeof( $validDependencies ) == 0 ) {
+			$sql = 'DELETE FROM '.self::TABLE_REQUIREMENTS." WHERE block_ID = {$blockID}";
+		} else {
+			$sql = 'DELETE FROM '.self::TABLE_REQUIREMENTS." WHERE block_ID = {$blockID} AND ID NOT IN (".implode( ',', $validDependencies ).');';
+		}
+
+		$this->database->exec( $sql );
 	}
 
 	/**
@@ -146,6 +136,7 @@ class BlocksGenerator
 		// remove unknown block
 		if( sizeof( $validBlocks ) == 0 ) {
 			$this->database->exec( 'DELETE FROM '.self::TABLE_BLOCKS );
+
 		} else {
 			$this->database->exec( 'DELETE FROM '.self::TABLE_BLOCKS.' WHERE ID NOT IN ('.implode( ',', $validBlocks ).');' );
 		}
@@ -162,74 +153,65 @@ class BlocksGenerator
 	{
 		echo "- registering template {$template->getPathname()} (block {$template->getParentBlock()})\n";
 
-		$this->database->beginTransaction();
+		$pathname = $template->getPathname();
+		// TODO make path relative to web root
+		// $pathname = substr( $pathname, sizeof($webRoot) );
 
-		try {
-			$pathname = $template->getPathname();
-			// TODO make path relative to web root
-			// $pathname = substr( $pathname, sizeof($webRoot) );
+		// load block
+		$dbs   = $this->database->query( 'SELECT ID FROM '.self::TABLE_BLOCKS.' WHERE code_name = '.$this->database->quote( $template->getParentBlock() ) );
+		$block = $dbs->fetch();
 
-			// load block
-			$dbs   = $this->database->query( 'SELECT ID FROM '.self::TABLE_BLOCKS.' WHERE code_name = '.$this->database->quote( $template->getParentBlock() ) );
-			$block = $dbs->fetch();
+		if( $block == null ) {
+			$this->database->commit();
+			return;
+		}
 
-			if( $block == null ) {
-				$this->database->commit();
-				return;
-			}
+		// if template with given name exists, reuse ID
+		if( isset( $existingTemplates[ $pathname ] ) ) {
+			$templateID = $existingTemplates[ $pathname ];
 
-			// if template with given name exists, reuse ID
-			if( isset( $existingTemplates[ $pathname ] ) ) {
-				$templateID = $existingTemplates[ $pathname ];
+		// otherwise insert new into DB
+		} else {
+			$this->database->exec( 'INSERT INTO '.self::TABLE_TEMPLATES." ( block_ID, filename ) VALUES ( {$block['ID']}, ".$this->database->quote( $pathname ).' )' );
+			$templateID = $this->database->lastInsertId();
+			$existingTemplates[ $pathname ] = $templateID;
+		}
+
+		$validTemplates[] = $templateID;
+
+		// SLOTS
+		$validSlots    = array();
+
+		$existingSlots = array();
+		$sql = 'SELECT ID, code_name FROM '.self::TABLE_SLOTS.' WHERE template_ID = '.$templateID;
+		foreach( $this->database->query( $sql ) as $r ) {
+			$existingSlots[ $r['code_name'] ] = $r['ID'];
+		}
+
+		// loop over associated slots
+		$slots = $template->getSlots();
+		foreach( $slots as $slotName ) {
+
+			// if slot with given name exists, reuse ID
+			if( isset( $existingSlots[ $slotName ] ) ) {
+				$slotID = $existingSlots[ $slotName ];
 
 			// otherwise insert new into DB
 			} else {
-				$this->database->exec( 'INSERT INTO '.self::TABLE_TEMPLATES." ( block_ID, filename ) VALUES ( {$block['ID']}, ".$this->database->quote( $pathname ).' )' );
-				$templateID = $this->database->lastInsertId();
-				$existingTemplates[ $pathname ] = $templateID;
+				$this->database->exec( 'INSERT INTO '.self::TABLE_SLOTS." ( template_ID, code_name ) VALUES ( {$templateID}, ".$this->database->quote( $slotName ).' )' );
+				$slotID = $this->database->lastInsertId();
 			}
 
-			$validTemplates[] = $templateID;
-
-			// SLOTS
-			$validSlots    = array();
-
-			$existingSlots = array();
-			$sql = 'SELECT ID, code_name FROM '.self::TABLE_SLOTS.' WHERE template_ID = '.$templateID;
-			foreach( $this->database->query( $sql ) as $r ) {
-				$existingSlots[ $r['code_name'] ] = $r['ID'];
-			}
-
-			// loop over associated slots
-			$slots = $template->getSlots();
-			foreach( $slots as $slotName ) {
-
-				// if slot with given name exists, reuse ID
-				if( isset( $existingSlots[ $slotName ] ) ) {
-					$slotID = $existingSlots[ $slotName ];
-
-				// otherwise insert new into DB
-				} else {
-					$this->database->exec( 'INSERT INTO '.self::TABLE_SLOTS." ( template_ID, code_name ) VALUES ( {$templateID}, ".$this->database->quote( $slotName ).' )' );
-					$slotID = $this->database->lastInsertId();
-				}
-
-				$validSlots[] = $slotID;
-			}
-
-			// remove unknown slots within template
-			if( sizeof( $validSlots ) == 0 )
-				$this->database->exec( 'DELETE FROM '.self::TABLE_SLOTS." WHERE template_ID = {$templateID};" );
-			else
-				$this->database->exec( 'DELETE FROM '.self::TABLE_SLOTS." WHERE template_ID = {$templateID} AND ID NOT IN (".implode( ',', $validSlots ).');' );
-
-
-		} catch( \DatabaseException $e ) {
-			$this->database->rollBack();
-			throw $e;
+			$validSlots[] = $slotID;
 		}
 
-		$this->database->commit();
+		// remove unknown slots within template
+		if( sizeof( $validSlots ) == 0 ) {
+			$this->database->exec( 'DELETE FROM '.self::TABLE_SLOTS." WHERE template_ID = {$templateID};" );
+
+		} else {
+			$this->database->exec( 'DELETE FROM '.self::TABLE_SLOTS." WHERE template_ID = {$templateID} AND ID NOT IN (".implode( ',', $validSlots ).');' );
+		}
 	}
 
 	/**
@@ -272,6 +254,5 @@ class BlocksGenerator
 		}
 
 		$this->database->exec( $sql );
-
 	}
 }
