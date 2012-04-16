61cbf12331fc8d2c16aedbe8bbf8e097fbc5d4ef
joker806 <joker806@gmail.com>
2012-04-16 15:44:45 +0200

    Remove unused dataGenerator

--- index_dataGenerator.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include_once 'common.php';
-
-$database = new \cDatabase( DATABASE );
-
-$admin = new \WebBuilder\cWebBuilderAdmin( $database );
-$admin->generateInstancesDataDependencies();
