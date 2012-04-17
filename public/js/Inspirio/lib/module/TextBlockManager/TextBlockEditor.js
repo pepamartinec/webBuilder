de5879cff225fa749393a1ba56fdf1291aaa5264
joker806 <joker806@gmail.com>
2012-04-17 16:07:58 +0200

    Included Twitter HTML5 bootstrap

---
+++ public/js/Inspirio/lib/module/TextBlockManager/TextBlockEditor.js
@@ -0,0 +1,66 @@
+Ext.define( 'Inspirio.module.TextBlockManager.TextBlockEditor',
+{
+	extend : 'Ext.form.Panel',
+
+	mixins : {
+		editor : 'extAdmin.component.editor.DataEditorFeature'
+	},
+
+	width : 800,
+
+	/**
+	 * Editor initialization
+	 *
+	 */
+	initComponent : function()
+	{
+		var me = this;
+
+		Ext.apply( me, {
+			border      : false,
+			bodyPadding : 5,
+
+			title   : 'Úprava textu',
+			iconCls : 'i-edit',
+
+			defaults : {
+				anchor : '100%'
+			},
+
+			items : [{
+				xtype : 'hiddenfield',
+				name  : 'ID'
+			},{
+				xtype      : 'textfield',
+				name       : 'title',
+				fieldLabel : 'Název'
+			},{
+				xtype      : 'htmleditor',
+				name       : 'content',
+				fieldLabel : 'Obsah',
+
+				height : 400
+			}]
+		});
+
+		me.mixins.editor.constructor.call( me );
+
+		me.callParent( arguments );
+	},
+
+	setData : function( data )
+	{
+		this.form.setValues( data );
+		return this;
+	},
+
+	getData : function()
+	{
+		return this.form.getValues();
+	},
+
+	isDirty : function()
+	{
+		return false;
+	}
+});
