a5a765f7246537f0f006780b9ca481d8892e763c
joker806 <joker806@gmail.com>
2012-05-02 11:20:43 +0200

    Added Pagodabox config

--- common.php
+++ common.php
@@ -1,18 +1,29 @@
 <?php
 
-define( 'HOST', 'localhost' );
-define( 'USER', 'root' );
-define( 'PASSWORD', '' );
-define( 'DATABASE', 'webBuilder' );
-define( 'AUTH_DATABASE_NAME', 'webBuilder' );
+// pagodabox
+if( isset( $_SERVER["DB1_HOST"] ) ) {
+	define( 'HOST', $_SERVER["DB1_HOST"] );
+	define( 'USER', $_SERVER["DB1_USER"] );
+	define( 'PASSWORD', $_SERVER["DB1_PASS"] );
+	define( 'DATABASE', $_SERVER["DB1_NAME"] );
 
+	define( 'BASE_HREF', 'http://web-builder.pagodabox.com/' );
+
+// localhost
+} else {
+	define( 'HOST', 'localhost' );
+	define( 'USER', 'root' );
+	define( 'PASSWORD', '' );
+	define( 'DATABASE', 'webBuilder' );
+
+	define( 'BASE_HREF', 'http://dev-local/webBuilder/' );
+}
+
+// server paths
 define( 'PATH_TO_ROOT', __DIR__.'/' );
 
-define( 'PATH_TO_WEBSERVER_ROOT', __DIR__.'/../' );
-define( 'PATH_FROM_ROOT_TO_BASE', 'webBuilder/' );
 
-define( 'BASE_HREF', 'http://dev-local/'. PATH_FROM_ROOT_TO_BASE );
-
+// activate composer autoloading
 require __DIR__.'/vendor/.composer/autoload.php';
 
 function exception_error_handler($errno, $errstr, $errfile, $errline ) {
@@ -21,11 +32,6 @@ function exception_error_handler($errno, $errstr, $errfile, $errline ) {
 set_error_handler("exception_error_handler");
 
 
-
-function safeInput( $what, $type = 'string' )
-{
-	return $what;
-}
 
 function determineLanguage()
 {
