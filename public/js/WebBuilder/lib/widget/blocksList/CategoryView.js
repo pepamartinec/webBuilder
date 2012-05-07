77b2555b1bd0f818cf329e81dbc8dde569fb0d05
joker806 <joker806@gmail.com>
2012-05-07 15:23:33 +0200

    Icon tweaks

--- public/js/WebBuilder/lib/widget/blocksList/CategoryView.js
+++ public/js/WebBuilder/lib/widget/blocksList/CategoryView.js
@@ -10,9 +10,19 @@ Ext.define( 'WebBuilder.widget.blocksList.CategoryView',
 
 	itemSelector : Ext.baseCSSPrefix +'template-block',
 
+	autoScroll : true,
+
 	tpl : [
 		'<tpl for=".">',
-			'<div class="', Ext.baseCSSPrefix ,'template-block ', Ext.baseCSSPrefix ,'template-block-{ID}">{[ values.title || values.codeName ]}</div>',
-		'</tpl>'
+			'<div class="', Ext.baseCSSPrefix ,'template-block ', Ext.baseCSSPrefix ,'template-block-{ID}">',
+				'<span class="', Ext.baseCSSPrefix ,'icon i-{[ this.getIconCls( values) ]}"></span>',
+				'{[ values.title || values.codeName ]}',
+			'</div>',
+		'</tpl>',
+		{
+			getIconCls : function( block ) {
+				return Ext.String.uncapitalize( block.codeName.split('\\').pop() );
+			}
+		}
 	]
 });
