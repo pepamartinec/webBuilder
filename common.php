af9b0f990a80291800f1ae6861fc319991b440f1
joker806 <joker806@gmail.com>
2012-05-07 21:17:18 +0200

    Cleaned common.php

--- common.php
+++ common.php
@@ -1,27 +1,19 @@
 <?php
 
-// pagodabox
-if( isset( $_SERVER["DB1_HOST"] ) ) {
-	define( 'HOST', $_SERVER["DB1_HOST"] );
-	define( 'USER', $_SERVER["DB1_USER"] );
-	define( 'PASSWORD', $_SERVER["DB1_PASS"] );
-	define( 'DATABASE', $_SERVER["DB1_NAME"] );
+// DATABASE CONNECTION
+define( 'HOST', 'localhost' );
+define( 'USER', 'root' );
+define( 'PASSWORD', '' );
+define( 'DATABASE', 'webBuilder' );
 
-	define( 'BASE_HREF', 'http://web-builder.pagodabox.com/' );
+// PATHS SETTINGS
+define( 'BASE_HREF', 'http://dev-local/webBuilder/' );
 
-// localhost
-} else {
-	define( 'HOST', 'localhost' );
-	define( 'USER', 'root' );
-	define( 'PASSWORD', '' );
-	define( 'DATABASE', 'webBuilder' );
 
-	define( 'BASE_HREF', 'http://dev-local/webBuilder/' );
-}
+
 
 // server paths
 define( 'PATH_TO_ROOT', __DIR__.'/' );
-
 
 // activate composer autoloading
 require __DIR__.'/vendor/.composer/autoload.php';
@@ -31,37 +23,6 @@ function exception_error_handler($errno, $errstr, $errfile, $errline ) {
 }
 set_error_handler("exception_error_handler");
 
-
-
-function determineLanguage()
-{
-	return 'cs';
-}
-
-function findCommonPrefix( array $strings )
-{
-	$leader = array_shift( $strings );
-
-	// find shortest string
-	$minLen = strlen( $leader );
-	foreach( $strings as $string ) {
-		$len = strlen( $string );
-
-		if( $len < $minLen ) {
-			$minLen = $len;
-		}
-	}
-
-	for( $i = 0; $i < $minLen; ++$i ) {
-		foreach( $strings as $string ) {
-			if( $string[ $i ] !== $leader[ $i ] ) {
-				return $i - 1;
-			}
-		}
-	}
-
-	return $minLen;
-}
 
 function redirect( $location )
 {
