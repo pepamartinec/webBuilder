64832c1cea8b720dfdf5c9af5d1126f5eaca007f
joker806 <joker806@gmail.com>
2012-06-11 15:45:46 +0200

    Extraction of the extAdmin administration into split project

--- public/js/WebBuilder/lib/widget/DataLinkField.js
+++
@@ -1,8 +0,0 @@
-Ext.define( 'WebBuilder.widget.DataLinkField', {
-	extend : 'Ext.form.field.Display',
-
-	valueToRaw : function( value )
-	{
-		return value.getProvider().block.get('title') +'::'+ value.getProperty() +'('+ value.getValue() +')';
-	}
-});
